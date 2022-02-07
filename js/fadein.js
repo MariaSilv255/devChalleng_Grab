64877364ed2e20ccf1e06eb864473e1696a3de35
maria aparecida <maria.info108@gmail.com>
2022-02-06 22:13:51 -0300

    Projeto concluido

---
+++ js/fadein.js
@@ -0,0 +1,36 @@
+
+function fadeIn(time){
+	var element = document.getElementById('sugir');
+	processa(element,time,0,100);
+}
+
+
+
+
+function processa(element,time,initial,end){
+	if(initial == 0){
+					 increment = 2;
+					 element.style.display = "block";
+	}else {
+					 increment = -2;
+	}
+
+
+
+	opc = initial;
+
+
+
+	intervalo = setInterval(function(){
+					 if((opc == end)){
+											if(end == 0){
+																			 element.style.display = "none";
+											}
+											clearInterval(intervalo);
+					 }else {
+											opc += increment;
+											element.style.opacity = opc/100;
+											element.style.filter = "alpha(opacity="+opc+")";
+					 }
+	},time * 10);
+			}
--- js/js.js
+++ js/js.js
@@ -1,15 +1,45 @@
-function validation(){
+function validation() {
 
-var usr ="teste@teste.com";
-var senha = "12345";
+  var email = document.getElementById('email').value;
+  var psw = document.getElementById('psw').value;
 
-var email = document.getElementById('email').value;
-var psw = document.getElementById('psw').value;
-
-if(usr === email && senha === psw){
-  window.location = "pagina.html";
-}else{
-  document.getElementById("email").setAttribute("required", ""); 
+  if (email === "teste@teste" && psw === "123") {
+    
+    alterarcor("b");
+    alert("Bem vindo(a) 🚀");
+    
+  } else {
+    alterarcor("a");
+    document.getElementById("error").innerHTML = "Por favor insirar um email valido!";
+    document.getElementById('psw').value="";
+    document.getElementById('email').value="";
+    //psw.style.cssText = 'border: 2px solid red;' ALTERAR VARIOS ATRIBUTOS CSS
+  }
 }
+  function alterarcor(tipo) {
+    var icon1 = document.getElementById("alteremail");
+    var icon2 = document.getElementById("lock");
+    var input = document.getElementById("input-email");
+    var input2 = document.getElementById("input-psw");
+    var email = document.getElementById('email');
+    
+    if (tipo === 'a') {
 
-}
+      icon1.style.cssText = 'color:red;'
+      icon2.style.cssText = 'color:red;'
+      input.style.cssText = 'border: 2px solid red;'
+      input2.style.cssText = 'border: 2px solid red;'
+      email.style.cssText = 'color:red;'
+
+
+    } else {
+
+      icon1.style.cssText = 'color:white;'
+      icon2.style.cssText = 'color:white;'
+      input.style.cssText = 'border: 2px solid white;'
+      input2.style.cssText = 'border: 2px solid white;'
+      email.style.cssText = 'color:white;'
+
+
+    }
+  }
